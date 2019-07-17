31e563413fd7bf308bfaeaaf3305f7197438b491
JohnJC <jonathanjcook@outlook.com>
2019-07-17 11:45:47 +1000

    test comment and updated gitignore file

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -14,6 +14,8 @@ import java.util.Map;
  */
 public class Main {
     private static StockList stockList = new StockList();
+    
+    // TEST COMMENT
 
     public static void main(String[] args) {
 	    StockItem temp = new StockItem("bread", 0.86, 100);
